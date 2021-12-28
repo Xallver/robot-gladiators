6b85d74391dddd8cf2b743177124f81ee093dbfd
Xallver <jamesstanley11@gmail.com>
2021-12-28 10:21:28 -0500

    fixed else stmt showing Game Over

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,7 +8,7 @@ console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
-var enemyAttack = 12;
+var enemyAttack = 50;
 
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
@@ -78,31 +78,22 @@ for (var i = 0; i < enemyNames.length; i++) {
   if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
     window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-  } else {
+
+
+    // pick new enemy to fight based on the index of the enemyNames array
+    var pickedEnemyName = enemyNames[i];
+
+    // reset enemyHealth before starting new fight
+    enemyHealth = 50;
+
+    // use debugger to pause script from running and check what's going on at that moment in the code
+    debugger;
+
+    // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+    fight(pickedEnemyName);
+  }
+  else {
     window.alert("You have lost your robot in battle! Game Over!");
     break;
   }
-
-  // pick new enemy to fight based on the index of the enemyNames array
-  var pickedEnemyName = enemyNames[i];
-
-  // reset enemyHealth before starting new fight
-  enemyHealth = 50;
-
-  // use debugger to pause script from running and check what's going on at that moment in the code
-  // debugger;
-
-  // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-  fight(pickedEnemyName);
 }
-
-
-// check to see if the value of the playerHealth variable is greater than 0
-if (playerHealth > 0) {
-  console.log("Your player is still alive!");
-}
-else {
-  (playerHealth <= 0)
-  console.log("Your player is dead!");
-}
-
